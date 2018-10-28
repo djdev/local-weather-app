65743708e630569983ad0faf7c4de6c0b159e49a
Deepjyoti Rajkhowa <deepjyot@netapp.com>
2018-10-27 22:32:55 -0400

    Add rxjs service call

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: 'e20309f8d7b4003a474118a66bca6616',
+  baseUrl: 'http://',
 };
 
 /*
